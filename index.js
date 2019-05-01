cc4db321a2a8602a4c7cab5d386f274a5d2c8c63
Joseph Esgar <josephesgar@Josephs-MacBook-Air.local>
2019-05-01 11:19:16 -0700

    Pretty Sad

---
+++ index.js
@@ -0,0 +1,48 @@
+console.dir(document);
+
+const myButton = document.getElementById("my-button");
+myButton.onclick = function(event) {
+  event.stopPropagation();
+  console.log("Hello from Js.");
+};
+
+document.body.onclick = function(e) {
+  console.log("the body was clicked");
+};
+
+document.body.onclick = function() {
+  console.log("the Second body event");
+};
+
+function myFunc() {
+  console.log("added to body through addEventListener");
+}
+document.body.addEventListener("click", myFunc);
+
+document.querySelector("form").onsubmit = submitMe;
+
+function submitMe(event) {
+  console.log(event);
+  event.preventDefault();
+  let eeail = document.querySelector("#email");
+  document.querySelector(".message").innerHTML = email.value;
+}
+
+function makeSomething() {
+  let newElem = document.createElement("a");
+  let text = document.createTextNode("Let me google that for you");
+  newElem.appendChild(text);
+  newElem.setAttribute("href", "https://lmgtfy.com");
+  newElem.setAttribute("class", "made-link");
+  document.body.appendChild(newElem);
+}
+
+document.querySelector(".add-link").onclick = makeSomething;
+
+function nukeSomething() {
+  let elemToRemove = document.querySelector(".made-link");
+  let parent = elemToRemove.parentNode;
+  parent.removeChild(elemToRemove);
+}
+
+document.querySelector(".remove-link").onclick = nukeSomething;
